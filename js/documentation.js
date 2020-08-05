bd52432a3603bf8e1c0679df8622211a555ff83c
Adam Robillard <hello@adamrobillard.ca>
2020-08-05 17:30:58 -0400

    Fix doc nav height issue

--- js/documentation.js
+++ js/documentation.js
@@ -2,4 +2,7 @@ console.log('Documentation Page');
 
 const navDocs = document.querySelector('.nav-docs');
 const navDocsList = document.querySelector('.nav-docs > ul');
-navDocs.setAttribute('style', `--height: ${navDocsList.offsetHeight}px`);
+
+if (window.innerWidth < 800) {
+  navDocs.setAttribute('style', `--height: ${navDocsList.offsetHeight}px`);
+}
